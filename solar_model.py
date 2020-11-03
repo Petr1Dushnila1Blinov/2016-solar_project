ce17b42675ceb2ef75b459cfa15d039b39e76f7f
Никита Матюхин <matiukhin.nd@phystech.ed>
2020-11-03 13:18:13 +0300

    ИÐFinally

--- solar_model.py
+++ solar_model.py
@@ -19,7 +19,7 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        F = (G*body.m*obj.m)/r**2
+        F = (G*body.m*obj.m)/(r**2)
         body.Fx += F*(obj.x-body.x)/r                          # FIXME: нужно вывести формулу...
         body.Fy += F*(obj.y-body.y)/r                          # FIXME: нужно вывести формулу...
 
